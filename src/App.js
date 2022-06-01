7e1d7e3de9220006e8edd441df6e7123879a5355
Chris Davitt <ChrisDavitt3@gmail.com>
2022-06-01 13:15:53 -0400

    Network Verification

--- src/App.js
+++ src/App.js
@@ -1,34 +1,87 @@
 import React, { useState } from 'react'
-import { ethers, getDefaultProvider } from 'ethers';
+import { ethers, providers, getDefaultProvider, EtherscanProvider } from 'ethers';
 import { useAppDispatch, useAppSelector } from './components/store/hooks';
 import { store } from './components/store/store';
 import { tokenABI } from './components/assets/helpers/tokenABI';
 
-export const App = () => {
 
-    const [errorMessage, setErrorMessage] = useState('');
+export const App = () => {
+    
+    const [errorMessage, setErrorMessage] = useState();
     const [connected, setConnected] = useState(false);
     const [defaultAccount, setDefaultAccount] = useState(null);
     const [userBalance, setUserBalance] = useState(0.0);
     const [connButtonText, setConnButtonText] = useState('Connect Wallet');
+    
+    const changeNetwork = async ({ networkName, setErrorMessage }) => {
+        try {
+            if (!window.ethereum) throw new Error('No Crypto Wallet Found');
+            await window.ethereum.request({
+                method: 'wallet_addEthereumChain',
+                params: [{
+                    ...networks[networkName]
+                }]
+            })
+        } catch (err) {
+            setErrorMessage(err.message);
+        }
+    }
+
+    const handleChainChange = () => {
+        window.location.reload()
+    }
 
     const count = store.getState()
     const dispatch = useAppDispatch();
 
-    const chainChangedHandler = () => {
-        window.location.reload()
+    const networks = {
+        polygon: {
+          chainId: `0x${Number(137).toString(16)}`,
+          chainName: "Polygon Mainnet",
+          nativeCurrency: {
+            name: "MATIC",
+            symbol: "MATIC",
+            decimals: 18
+          },
+          rpcUrls: ["https://polygon-rpc.com/"],
+          blockExplorerUrls: ["https://polygonscan.com/"]
+        },
+        rinkeby: {
+          chainId: `0x${Number(4).toString(16)}`,
+          chainName: "Ethereum Testnet Rinkeby",
+          nativeCurrency: {
+            name: "Rinkeby Ether",
+            symbol: "RIN",
+            decimals: 18
+          },
+          rpcUrls: [
+            "https://rinkeby.infura.io/v3/${INFURA_API_KEY}",
+            "wss://rinkeby.infura.io/ws/v3/${INFURA_API_KEY}"
+            ],
+          blockExplorerUrls: ["https://rinkeby.etherscan.io"]
+        }
+      };
+
+    const handleNetworkSwitch = async (networkName) => {
+        setErrorMessage();
+        await changeNetwork({ networkName, setErrorMessage })
     }
-    
+
     const connectWalletHandler = () => {
         if (connected === false){
             if (window.ethereum) {
-                window.ethereum.request({method: 'eth_requestAccounts'})
-                .then(result => {
-                    setDefaultAccount(result[0]);
-                    getUserBalance(result[0]);
-                    setConnButtonText(result[0].slice(0,6)+ '...');
-                    setConnected(true);
-                }) 
+                if (window.ethereum.networkVersion != 137) {
+                    handleNetworkSwitch('polygon')
+                } else {
+                    window.ethereum.request({method: 'eth_requestAccounts'})
+                    .then(result => {
+                        setDefaultAccount(result[0]);
+                        getUserBalance(result[0]);
+                        setConnButtonText(result[0].slice(0,6)+ '...');
+                        setConnected(true);
+                        console.log('Account Updated')
+                    })
+                } 
             } else {
                 setConnected(false);
                 setDefaultAccount('');
@@ -47,8 +100,8 @@ export const App = () => {
 
     const getUserBalance = (address) => {
         const ethers = require('ethers')
-        const network = 'rinkeby' // use rinkeby testnet
-        const provider = ethers.getDefaultProvider(network)
+        const network = 'matic'
+        const provider = new ethers.providers.InfuraProvider(network)
         provider.getBalance(address).then((balance) => {
             // convert a currency unit from wei to ether
             const balanceInEth = ethers.utils.formatEther(balance)
@@ -65,13 +118,9 @@ export const App = () => {
     //         console.log('Retrieving Wallet Contents')
     //         console.log(count)
     //     });
-    
-    store.subscribe(() => {
-        console.log("current state", store.getState().items);
-    });
   
     window.ethereum.on('accountsChanged', connectWalletHandler)
-    window.ethereum.on('chainChanged', chainChangedHandler)
+    window.ethereum.on('chainChanged', handleChainChange)
 
     return (
         
