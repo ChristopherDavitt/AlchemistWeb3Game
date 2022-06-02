d577f324facea54948238623646059439eefeb37
Chris Davitt <ChrisDavitt3@gmail.com>
2022-06-01 22:23:04 -0400

    Added wallet stuff / Contract Ideas

--- src/App.js
+++ src/App.js
@@ -46,19 +46,20 @@ export const App = () => {
           rpcUrls: ["https://polygon-rpc.com/"],
           blockExplorerUrls: ["https://polygonscan.com/"]
         },
-        rinkeby: {
-          chainId: `0x${Number(4).toString(16)}`,
-          chainName: "Ethereum Testnet Rinkeby",
+        mumbai: {
+          chainId: `0x${Number(80001).toString(16)}`,
+          chainName: "Polygon Testnet Mumbai",
           nativeCurrency: {
-            name: "Rinkeby Ether",
-            symbol: "RIN",
+            name: "MATIC",
+            symbol: "MATIC",
             decimals: 18
           },
           rpcUrls: [
-            "https://rinkeby.infura.io/v3/${INFURA_API_KEY}",
-            "wss://rinkeby.infura.io/ws/v3/${INFURA_API_KEY}"
+            "https://matic-mumbai.chainstacklabs.com",
+            "https://rpc-mumbai.maticvigil.com",
+            "https://matic-testnet-archive-rpc.bwarelabs.com"
             ],
-          blockExplorerUrls: ["https://rinkeby.etherscan.io"]
+          blockExplorerUrls: ["https://mumbai.polygonscan.com"]
         }
       };
 
